d46b59db26f3fc97b93d9ba5146534b3373e3ee6
romanusbett <bettromanus@gmail.com>
2025-09-29 12:49:46 +0300

    add admin site

--- frontend/src/app/components/info-cards/info-cards.component.ts
+++ frontend/src/app/components/info-cards/info-cards.component.ts
@@ -1,9 +1,12 @@
+
 import { Component } from '@angular/core';
 import { input } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+import { LucideIconData } from 'lucide-angular';
 
 @Component({
   selector: 'app-info-cards',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './info-cards.component.html',
   styles: ``
 })
@@ -11,5 +14,5 @@ export class InfoCardsComponent {
   title = input<string>();
   figure = input<number>();
   label = input<string>();
-
+  icon = input<LucideIconData>();
 }
--- frontend/src/app/pages/client-p/client-p.component.ts
+++ frontend/src/app/pages/client-p/client-p.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { InfoCardsComponent } from '../../components/info-cards/info-cards.component';
 import { MiniNavComponent } from '../../components/mini-nav/mini-nav.component';
-
+import { VehicleCardsComponent } from '../../components/vehicle-cards/vehicle-cards.component';
+import { ServiceCardsComponent } from '../../components/service-cards/service-cards.component';
+import { Car, DollarSign, Calendar } from 'lucide-angular';
 
 @Component({
   selector: 'app-client-p',
-  imports: [NavbarComponent, InfoCardsComponent, MiniNavComponent],
+  imports: [NavbarComponent, InfoCardsComponent, MiniNavComponent, VehicleCardsComponent, ServiceCardsComponent],
   templateUrl: './client-p.component.html',
   styles: ``
 })
@@ -15,20 +17,73 @@ export class ClientPComponent {
     {
       cardTitle: "My cars",
       cardFigures: 2,
-      cardLabels:"Registered Vehicles" 
+      cardLabels:"Registered Vehicles",
+      cardIcon: Car,
     },
     {
       cardTitle: "Total Services",
       cardFigures: 2,
-      cardLabels:"All time Services" 
+      cardLabels:"All time Services",
+      cardIcon: Calendar,
     },
     {
       cardTitle: "Total Spent",
       cardFigures: 1500,
-      cardLabels:"Completed Services" 
+      cardLabels:"Completed Services",
+      cardIcon: DollarSign,
     }
   ]
   statesToBeToggled = ['My Cars', 'Service History'];
+
+  carCards = [
+    {
+      id:1,
+      carType: "Toyota Camry", 
+      year: "2020",
+      colour: "Silver",
+      License: "KAA001",
+      services: 1,
+    },
+    {
+      id:2,
+      carType: "Subaru Impreza", 
+      year: "2000",
+      colour: "Black",
+      License: "KBA001",
+      services: 2,
+    },
+    {
+      id:3,
+      carType: "Porshe Cayenne", 
+      year: "2014",
+      colour: "Green",
+      License: "KCA001X",
+      services: 1,
+    }
+  ]
+
+  serviceCardsInfo = [
+    {
+      id: 1,
+      type: "Basic Wash",
+      carType: "Subaru Impreza", 
+      year: "2000",
+      License: "KBA001",
+      serviceDate: " 27/09/2025",
+      status: "In-Progress",
+      price: 2000,
+    },
+    {
+      id:2,
+      type: "Premium Wash & Wax",
+      carType: "Porshe Cayenne", 
+      year: "2019",
+      License: "KCA001X",
+      serviceDate: " 27/09/2025",
+      status: "completed",
+      price: 4000,
+    }
+  ]
 
   state = 'My Cars';
 
